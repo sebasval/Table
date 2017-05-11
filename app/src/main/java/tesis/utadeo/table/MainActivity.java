2227d61e2382131bfed52fb66f0a73f745a3b757
sebasval <s.dvh27@gmail.com>
2017-05-11 15:01:24 -0500

    Content Navigation Drawer Complete

--- app/src/main/java/tesis/utadeo/table/MainActivity.java
+++ app/src/main/java/tesis/utadeo/table/MainActivity.java
@@ -25,6 +25,7 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Noticias");
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
