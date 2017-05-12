99f8a5426caf66e3da85461467145f5e1a51e327
sebasval <s.dvh27@gmail.com>
2017-05-11 19:49:30 -0500

    Loggin Complete and Header Complete

--- app/src/main/java/tesis/utadeo/table/MainActivity.java
+++ app/src/main/java/tesis/utadeo/table/MainActivity.java
@@ -3,9 +3,8 @@ package tesis.utadeo.table;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -13,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.facebook.AccessToken;
 import com.facebook.login.LoginManager;
@@ -46,6 +46,7 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
 
         //Verificacion Login
         if (AccessToken.getCurrentAccessToken() == null){
